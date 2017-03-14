b648213748475eec32880d15e32fbf80b1286f5e
varunkoppana <varunkoppana41@gmail.com>
2017-03-14 11:25:37 -0400

    Update parentheses.java

--- parentheses.java
+++ parentheses.java
@@ -3,38 +3,51 @@ package stack;
 import java.util.*;
 import java.util.Scanner;
 
-
- class solution {
- 
+class solution
+{
 	public static char[][] ARRAYLIST = {{'{','}'},{'[',']'},{'<','>'}};
 	
-	public static boolean isOpen(char c){
-		for(char[] array : ARRAYLIST){
-			if (array[0]==c){
+	public static boolean isOpen(char c)
+	{
+		for(char[] array : ARRAYLIST)
+		{
+			if (array[0]==c)
+			{
 				return true;
 			}
 		}
 		return false;
 	}
-		public static boolean matche(char open,char close){
-			for(char[] array : ARRAYLIST){
-				if (array[0]==open){
-					return array[1]==close;
-				}
+	
+	public static boolean matche(char open,char close)
+	{
+		for(char[] array : ARRAYLIST)
+		{
+			if (array[0]==open)
+			{
+				return array[1]==close;
+			}
 		}
-			return false;
+		return false;
 	}
 	
-	public static boolean parenBalance(String input){
+	public static boolean parenBalance(String input)
+	{
 		Stack<Character> stack = new Stack<Character>();
-		for(char c : input.toCharArray()){                //string to character
-			if(isOpen(c)){
+		for(char c : input.toCharArray())
+		{                
+			if(isOpen(c))
+			{
 				stack.push(c);
-			}else{
-				if(stack.isEmpty()|| !matche(stack.peek(),c)){
+			}
+			else
+			{
+				if(stack.isEmpty()|| !matche(stack.peek(),c))
+				{
 					return false;
-				
-				}else {
+				}
+				else 
+				{
 					stack.pop();
 				}
 			}
@@ -42,14 +55,14 @@ import java.util.Scanner;
 		return stack.isEmpty();
 	}
 }
-public class parentheses{
-	public static void main(String[] args) {
+public class parentheses
+{
+	public static void main(String[] args)
+	{
 		solution s = new solution();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the string");
 		String exp = sc.next();
 		System.out.println(solution.parenBalance(exp));
-
 	}
-
 }
